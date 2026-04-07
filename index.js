2f06965fae7bfe7daf80f897404ca9b5db46ad63
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-07 21:54:06 +0600

    created server and implement mongoose model Schema

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// mongoose configuration BY ATLAS
+// async function main() {
+//   await mongoose.connect(
+//     "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.6prdapd.mongodb.net/?appName=Cluster0",
+//   );
+// }
+
+// mongoose configuration BY MONGODB SOFTWARE
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+
+  app.get("/", (req, res) => {
+    res.send("Meta Blog Server Is Running!");
+  });
+}
+
+main()
+  .then(() => console.log("Mongodb Connected Successfully"))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server is Running On Port ${port}`);
+});
---
+++ src/models/blog.models.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    image: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model("Blog", BlogSchema);
+
+module.exports = Blog;
