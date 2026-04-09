f6cf17a24424573511515914c7ad8b1fd0f98de6
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-04-09 21:25:45 +0600

    implement sort feature to blog routes

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -2,18 +2,29 @@ const express = require("express");
 const Blog = require("../models/blog.models");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This is the blogs section");
+router.get("/", async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    // console.log(blogs);
+    res.status(201).send({ message: "Blog data fetch successfully", blogs });
+  } catch (error) {
+    console.log("Fetching Data Error", error);
+    res.status(500).send({ message: "Fetching Data Error", error });
+  }
 });
 
 // post a new blogs
 router.post("/add-post", async (req, res) => {
-  const newBlog = new Blog({
-    ...req.body,
-  });
-
-  const blog = await newBlog.save();
-  res.status(200).send({ message: "Post is created Successfully", blog });
+  try {
+    const blog = new Blog({
+      ...req.body,
+    });
+    await blog.save();
+    res.status(201).send({ message: "Creating blog Successfully", blog });
+  } catch (error) {
+    console.log("Error Creating blog", error);
+    res.status(500).send({ message: "Error Creating Blog", error });
+  }
 });
 
 module.exports = router;
